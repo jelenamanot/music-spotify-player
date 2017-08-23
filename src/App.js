af0fa1e0375b2b73467ed51095c9dc48771e9841
Jelena Nesic <jelenanesicm@gmail.com>
2017-08-23 12:31:50 +0200

    First Commit

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Profile from './Profile';
+import Gallery from './Gallery';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      artist: null, //IMPORTANT
+      tracks: []
+    }
+  }
+
+
+  // MAIN SEARCH FUNCTION
+  search() {
+    //-----------API SETUP-----------
+    const BASE_URL = 'https://api.spotify.com/v1/search?';
+    const FETCH_URL = BASE_URL + 'q=' + this.state.query + '&type=artist&limit=1';
+    //const ALBUM_URL = 'https://api.spotify.com/v1/artists/';
+
+    /*
+      accessToken expires every one hour. 
+      In order to get accessToken start "web-api-auth-examples"
+    */
+    let accessToken = 'BQCkkNY0DJ3_Pokg00AE7SbA520IgLqHGNMc_ob0nPUqVbC_I3pVBV-JdjLe_8kLDAPGXM-MwcwPEAy3H5995Y_G07snPILYxWOeRrIsxs82l1WtdiyjmubpNBUo0FeSVbFsAIQ9A6yRRpAd7fTw4-L83QNaroz5_fk&refresh_token=AQAZwgDWcX5SjKOj21bFYBKrDbkMVYczc8F0UUjRrql5SK4HXX7wEX7haAVjaNOhHadcrDAFPMNlik9nLesNKmMkNRn3WIMknqhry8TccYIxo1nE9WwIz9bwcLGHkKVH5p8';
+
+    let myOptions = {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + accessToken
+      },
+      mode: 'cors',
+      cache: 'default'
+    };
+
+    // FETCH!!!!
+    fetch(FETCH_URL, myOptions)
+    .then(response => response.json())
+    .then(json => {
+      const artist = json.artists.items[0];        
+      this.setState({ artist });
+      
+    /*  
+      FETCH_URL = `${ALBUM_URL}${artist.id}/top-tracks?country=US&`
+      fetch(FETCH_URL, myOptions) 
+      .then(response => response.json())
+      .then(json => {
+        console.log('artist\'s top tracks:', json);
+        const { tracks } = json;
+        this.setState({tracks});
+      })
+    */
+
+    });
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>&#9733; Music Spotify Player &#9733;</h2>
+
+          <div className="InputGroup">
+            <input
+              type="text"
+              placeholder="Search for an artist"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress={
+                event => {
+                  if(event.key === 'Enter') {
+                    this.search()
+                  }
+                }
+              }
+            />
+            <button onClick={ () => this.search()}>
+              <img src={require('./img/search.png')} alt="search"/>
+            </button>
+          </div>
+
+          {
+            this.state.artist !== null 
+            ? <div>
+                <Profile
+                  artist={this.state.artist}
+                />
+              </div>
+            : <p></p>
+          }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Gallery.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Gallery.css';
+
+class Gallery extends Component {
+  render() {
+    return(
+      <div className="Gallery">
+      <p>gallery of top 10 tracks here.</p>
+      </div>
+    );
+  }
+}
+
+export default Gallery;
